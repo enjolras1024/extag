fe3b93e8add4c2c513e56bbc3bc585cf139c37b3
Chen Junyong <enjolras1024@163.com>
2020-04-25 17:07:54 +0800

    chore: use gulp with rollup

---
+++ gulpfile.js
@@ -0,0 +1,110 @@
+const gulp = require('gulp');
+const rollup = require('rollup');
+const alias = require('rollup-plugin-alias');
+const uglify = require('rollup-plugin-uglify');
+const replace = require('rollup-plugin-replace');
+const pkgjson = require('./package.json');
+
+const version = pkgjson.version;
+
+async function build(opts) {
+  const format = opts.format || 'umd';
+  const input = opts.input;
+  const name = opts.name;
+  const file = opts.file;
+  const banner = `
+/**
+ * ${name} v${version}
+ * (c) enjolras.chen
+ * Released under the MIT License.
+ */
+  `;
+
+  let plugins = [
+    alias({
+      src: `${__dirname}/src`
+    }),
+    replace({
+      delimiters: ['', ''],
+      '//#test': file.lastIndexOf('.test') > 0 ? '' : '//',
+      '__ENV__': '"development"',
+      '__VERSION__': `"${version}"`
+    })
+  ];
+
+  let bundle = await rollup.rollup({
+    input: `src/${input}`,
+    plugins: plugins
+  });
+
+  await bundle.write({
+    name: name,
+    format: format,
+    file: `dist/${file}`,
+    banner: banner.trim()
+  });
+
+  let i = file.lastIndexOf('.');
+  let minfile = file.slice(0, i) + '.min' + file.slice(i);
+
+  plugins = [
+    alias({
+      src: `${__dirname}/src`
+    }),
+    replace({
+      delimiters: ['', ''],
+      '//#test': file.lastIndexOf('.test') > 0 ? '' : '//',
+      '__ENV__': '"production"',
+      '__VERSION__': `"${version}"`
+    }),
+    uglify.uglify()
+  ]
+
+  bundle = await rollup.rollup({
+    input: `src/${input}`,
+    plugins: plugins
+  });
+
+  await bundle.write({
+    name: name,
+    format: format,
+    file: `dist/${minfile}`
+  });
+}
+
+
+gulp.task('rollup-extag', async function () {
+  await build({
+    format: 'umd',
+    input: 'Extag.js',
+    file: 'extag.js',
+    name: 'Extag'
+  })
+});
+
+gulp.task('rollup-extag-test', async function () {
+  await build({
+    format: 'umd',
+    input: 'Extag.js',
+    file: 'extag.test.js',
+    name: 'Extag'
+  })
+});
+
+gulp.task('rollup-extag-dom', async function () {
+  await build({
+    format: 'umd',
+    input: 'views/ExtagDom.js',
+    file: 'extag-dom.js',
+    name: 'ExtagDom'
+  })
+});
+
+gulp.task('rollup-extag-ssr', async function () {
+  await build({
+    format: 'umd',
+    input: 'views/ExtagSSR.js',
+    file: 'extag-ssr.js',
+    name: 'ExtagSSR'
+  })
+});
